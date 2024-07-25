6a91c406b8faec2984522aceb14480f3690fa72f
Christine <156078562+christine2377@users.noreply.github.com>
2024-07-25 10:22:41 +0200

    Create Présentation.py

---
+++ Présentation.py
@@ -0,0 +1,32 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title="ORA 2024",
+    page_icon="	:chart_with_upwards_trend:",
+    layout="wide")
+
+
+st.markdown("# L'Opinion des Responsables Associatifs en 2024")
+st.sidebar.markdown("# Présentation")
+
+("")
+("Enquête en ligne du ---------- 2024, auprès de 2716 dirigeants d'association de profils et d'horizons diversifiés. "
+ "Cet échantillon robuste permet de réaliser des analyses croisées et de tenir compte de la diversité des situations et des formes d'engement.")
+
+("En cliquant dans le menu à gauche vous pouvez télécharger les résultats par secteur d'activités et par tranche d'âges. Ils viennent en complément de ceux présentés dans *La France bénévole* "
+ "*2024*, en ligne sur www.recherches-solidarites.org")
+
+("D’autres résultats sont disponibles, sur demande, auprès de [Marie](mailto:marie.duros@recherches-solidarites.org).")
+
+("")
+("")
+("")
+
+("*---------Cette enquête du Baromètre d’Opinion des Bénévoles de Recherches & Solidarités est réalisée, cette année, en coopération avec l’Institut Européen "
+ "de Développement Humain. Elle a bénéficié d’une large diffusion grâce à la mobilisation des partenaires suivants :*")
+
+
