c1e0e3f075a2ad6d934976fa14bc085a77ed2fe1
Christine <156078562+christine2377@users.noreply.github.com>
2024-07-25 14:48:24 +0200

    Update Présentation.py

--- Présentation.py
+++ Présentation.py
@@ -9,24 +9,26 @@ st.set_page_config(
     page_icon="	:chart_with_upwards_trend:",
     layout="wide")
 
+st.image('ORA2024.png',width = 500)
 
-st.markdown("# L'Opinion des Responsables Associatifs en 2024")
+st.markdown("# L'Opinion des Responsables Associatifs face aux enjeux liés à la transition écologique")
 st.sidebar.markdown("# Présentation")
 
 ("")
-("Enquête en ligne du ---------- 2024, auprès de 2716 dirigeants d'association de profils et d'horizons diversifiés. "
- "Cet échantillon robuste permet de réaliser des analyses croisées et de tenir compte de la diversité des situations et des formes d'engement.")
+("Enquête en ligne du 25 avril au 2 juillet 2024, auprès de 2716 dirigeants d'associations de toutes tailles, de tous secteurs et de toutes régions. ")
 
-("En cliquant dans le menu à gauche vous pouvez télécharger les résultats par secteur d'activités et par tranche d'âges. Ils viennent en complément de ceux présentés dans *La France bénévole* "
- "*2024*, en ligne sur www.recherches-solidarites.org")
+("Résultats des non-employeurs redressés selon la méthode des quotas appliquée aux variables « secteur d’activité » et « budget » d’après les données du *Paysage associatif français* – 4ème édition 2023 – hors série Juris Associations.")
+
+("Résultats des employeurs redressés selon la méthode des quotas appliquée aux variables « secteur d’activité » et « tranche d’effectifs salariés », d’après les données 2023 de l’URSSAF Caisse nationale et de la Mutualité Sociale Agricole")
+("Ces échantillons robustes permettent de réaliser des analyses croisées et de tenir compte de la diversité du secteur associatif.")
+
+("En cliquant dans le menu à gauche vous pouvez télécharger les résultats selon différents critères.")
 
 ("D’autres résultats sont disponibles, sur demande, auprès de [Marie](mailto:marie.duros@recherches-solidarites.org).")
 
-("")
-("")
-("")
 
-("*---------Cette enquête du Baromètre d’Opinion des Bénévoles de Recherches & Solidarités est réalisée, cette année, en coopération avec l’Institut Européen "
- "de Développement Humain. Elle a bénéficié d’une large diffusion grâce à la mobilisation des partenaires suivants :*")
+
+
+
 
 
