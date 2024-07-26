dc4c526b91f16c53ab960bdaf630cd53bca0ccc4
Christine <156078562+christine2377@users.noreply.github.com>
2024-07-26 16:31:37 +0200

    Update Par_territoires.py

--- pages/Par_territoires.py
+++ pages/Par_territoires.py
@@ -32,11 +32,12 @@ with tab1:
          "En zone péri-urbaine",
          "En zone urbaine",
          "Dans un quartier politique de la ville",
-         "En ZFRR"))
+         "En ZFRR*"))
 
     # Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?
     table = pd.read_excel(fichier, sheet_name=sheet, skiprows=9, nrows=9, index_col=0)
     table = table.applymap(lambda x: f'{x * 100:.0f}%')
+    table = table.rename(columns = {"En ZFRR" : "En ZFRR*"})
     slice = [option]
     forme = table.style.set_properties(**{'background-color' : 'cornflowerblue', 'text-align': 'center', 'color' : "white"}, subset = slice)
     st.table(forme)
@@ -45,6 +46,7 @@ with tab1:
 
     # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
     table1 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=87,nrows= 3, index_col =0)
+    table1 = table1.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table1[option]*100
     names = fig_col.index
     st.header("Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?")
@@ -56,6 +58,7 @@ with tab1:
 
     # La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)
     table2 = pd.read_excel(fichier, sheet_name=sheet, skiprows=95, nrows=3, index_col=0)
+    table2 = table2.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table2[option] * 100
     names = fig_col.index
     st.subheader("La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)")
@@ -66,6 +69,7 @@ with tab1:
 
     # La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)
     table3 = pd.read_excel(fichier, sheet_name=sheet, skiprows=103, nrows=3, index_col=0)
+    table3 = table3.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table3[option] * 100
     names = fig_col.index
     st.subheader("La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)")
@@ -76,6 +80,7 @@ with tab1:
 
     # Des achats responsables (en local, circuit-court...)
     table4 = pd.read_excel(fichier, sheet_name=sheet, skiprows=111, nrows=3, index_col=0)
+    table4 = table4.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table4[option] * 100
     names = fig_col.index
     st.subheader("Des achats responsables (en local, circuit-court...)")
@@ -86,6 +91,7 @@ with tab1:
 
     # Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)
     table5 = pd.read_excel(fichier, sheet_name=sheet, skiprows=119, nrows=3, index_col=0)
+    table5 = table5.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table5[option] * 100
     names = fig_col.index
     st.subheader("Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)")
@@ -96,6 +102,7 @@ with tab1:
 
     # Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale
     table6 = pd.read_excel(fichier, sheet_name=sheet, skiprows=127, nrows=3, index_col=0)
+    table6 = table6.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table6[option] * 100
     names = fig_col.index
     st.subheader("Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale")
@@ -106,6 +113,7 @@ with tab1:
 
     # La sobriété numérique (utilisation durable et raisonnable du numérique)
     table7 = pd.read_excel(fichier, sheet_name=sheet, skiprows=135, nrows=3, index_col=0)
+    table7 = table7.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table7[option] * 100
     names = fig_col.index
     st.subheader("La sobriété numérique (utilisation durable et raisonnable du numérique)")
@@ -136,6 +144,7 @@ with tab1:
     st.pyplot(fig)
 
     table8 = pd.read_excel(fichier, sheet_name=sheet, skiprows=147, nrows=9, index_col=0)
+    table8 = table8.rename(columns={"En ZFRR": "En ZFRR*"})
     fig_col = table8[option]*100
     fig_col.sort_values(ascending= True, inplace = True)
     fig, ax = plt.subplots()
@@ -149,45 +158,54 @@ with tab1:
 
 # Votre association prend-elle en compte les enjeux liés à la transition écologique pour mener à bien ses activités et organiser son action ?
 table = pd.read_excel( fichier, sheet_name = sheet ,skiprows=9,nrows= 9, index_col =0, dtype = "object")
+table = table.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table = table.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #Les économies d'énergie (électricité, gaz,...) et de la ressource en eau
 table1 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=87,nrows= 5, index_col =0, dtype = "object")
+table1 = table1.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table1 = table1.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #La limitation des déplacements, les transports collectifs et les mobilités douces (vélo…)
 table2 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=95,nrows= 5, index_col =0, dtype = "object")
+table2 = table2.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table2 = table2.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #La gestion des déchets (tri sélectif, moins d'emballage, biodéchets...)
 table3 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=103,nrows= 5, index_col =0, dtype = "object")
+table3 = table3.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table3 = table3.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #Des achats responsables (en local, circuit-court...)
 table4 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=111,nrows= 5, index_col =0, dtype = "object")
+table4 = table4.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table4 = table4.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #Le recours à des fournitures plus écologiques (papier recyclé, cartouches d'encre rechargeables...)
 table5 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=119,nrows= 5, index_col =0, dtype = "object")
+table5 = table5.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table5 = table5.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #Le réemploi, le recours aux recycleries et aux entreprises d'insertion à vocation environnementale
 table6 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=127,nrows= 5, index_col =0, dtype = "object")
+table6 = table6.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table6 = table6.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Quelle attention porte votre association aux pratiques suivantes dans la conduite de ses activités et dans son organisation ?
 #La sobriété numérique (utilisation durable et raisonnable du numérique)
 table7 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=135,nrows= 5, index_col =0, dtype = "object")
+table7 = table7.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table7 = table7.applymap(lambda x: f'{x * 100:.0f}%')
 
 # Qu'est-ce qui pourrait aider votre association à [mieux] prendre en compte les enjeux liés à la transition écologique dans ses activités et son fonctionnement ? Plusieurs réponses possibles
 table8 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=147,nrows= 9, index_col =0, dtype = "object")
+table8 = table8.rename(columns = {"En ZFRR" : "En ZFRR*"})
 table8 = table8.applymap(lambda x: f'{x * 100:.0f}%')
 
 styled_table = table.style.set_properties(**{'text-align': 'center'})
